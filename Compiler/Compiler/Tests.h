b2b1336beb74ee545d06fcd4f9d299dab8673bf7
Vadik <ivadik2323@gmail.com>
2017-03-14 19:57:42 +1000

    Tests.h: regroup includes

--- Compiler/Compiler/Tests.h
+++ Compiler/Compiler/Tests.h
@@ -1,9 +1,9 @@
 #pragma once
-#include "gtest\gtest.h"
 #include <fstream>
 #include <string>
-#include "Scanner.h"
 
+#include "gtest\gtest.h"
+#include "Scanner.h"
 
 class ScannerTest : public ::testing::Test {
 protected:
